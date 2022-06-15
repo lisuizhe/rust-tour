5e56591cb14dbbec2b4897fc40814c4f8acd2988
lisuizhe <suizhe.li@outlook.com>
2022-06-15 10:27:40 +0900

    server: response

--- learn-cargo-by-building-real-applications/server/src/server.rs
+++ learn-cargo-by-building-real-applications/server/src/server.rs
@@ -1,6 +1,6 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::{TryFrom};
-use std::io::Read;
+use std::io::{Read};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -27,11 +27,26 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
-                                    println!("request = {:#?}", request);
+                                    dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>IT WORKS!</h1>".to_string()),
+                                    )
                                 }, 
-                                Err(e) => println!("Failed to parse request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse request: {}", e);
+                                    Response::new(
+                                        StatusCode::BadRequest,
+                                        None,
+                                    )
+                                },
+                            };
+
+                             
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         },
                         Err(e) => println!("Failed to read from connection: {}", e),
