c568cfeef3eedbd990e485b600631cc039994511
lisuizhe <suizhe.li@outlook.com>
2022-06-18 21:22:31 +0900

    while and loop

---
+++ while-and-loop/src/main.rs
@@ -0,0 +1,25 @@
+fn while_and_loop() {
+    let mut x = 1;
+
+    while x < 1000 {
+        x *= 2;
+        if x == 64 {
+            continue;
+        }
+        println!("x: {}", x);
+    }
+
+    let mut y = 1;
+    loop {
+        y *= 2;
+        println!("y: {}", y);
+
+        if y == 1 << 10 {
+            break;
+        }
+    }
+}
+
+fn main() {
+    while_and_loop();
+}
