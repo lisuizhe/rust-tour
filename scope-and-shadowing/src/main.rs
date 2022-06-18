f73215299c46873117ab31cfbf48d011471f7651
lisuizhe <suizhe.li@outlook.com>
2022-06-18 17:49:45 +0900

    scope and shadowing

---
+++ scope-and-shadowing/src/main.rs
@@ -0,0 +1,19 @@
+fn scope_and_shadowing() {
+    let a = 123;
+
+    {
+        let b = 456;
+        println!("inside, b = {}", b);
+
+        let a = 777;
+        println!("inside, a = {}", a);
+    }
+
+    println!("outside, a = {}", a);
+
+    //println!("outside, b = {}", b);
+}
+
+fn main() {
+    scope_and_shadowing();
+}
