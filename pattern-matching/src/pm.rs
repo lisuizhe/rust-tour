bf291508ef5ccd2586712ce2d0bdb9d468f480df
lisuizhe <suizhe.li@outlook.com>
2022-06-19 07:41:00 +0900

    pattern matching

---
+++ pattern-matching/src/pm.rs
@@ -0,0 +1,24 @@
+fn how_many(x: i32) -> &'static str {
+    match x {
+        0 => "no",
+        1 | 2 => "one or two",
+        9..=11 => "lots of",
+        12 => "a dozen",
+        _ if (x % 2 == 0) => "some",
+        _ => "a few",
+    }
+}
+
+pub fn pattern_matching() {
+    for x in 0..13 {
+        println!("{}: I have {} organges", x, how_many(x));
+    }
+
+    let point = (3, 7);
+    match point {
+        (0, 0) => println!("origin"),
+        (0, y) => println!("x axis, y = {}", y),
+        (ref x, 0) => println!("y axis, x = {}", x),
+        (x, y) => println!("({}, {})", x, y),
+    }
+}
