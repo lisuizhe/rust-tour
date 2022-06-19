e122f46dd0a9b917d103cd124bd442d8561d6bfa
lisuizhe <suizhe.li@outlook.com>
2022-06-19 17:47:48 +0900

    drop

---
+++ drop/src/main.rs
@@ -0,0 +1,29 @@
+#![allow(warnings)]
+
+struct Creature {
+    name: String,
+}
+
+impl Creature {
+    fn new(name: &str) -> Creature {
+        println!("{} enters the game", name);
+        Creature { name: name.into() }
+    }
+}
+
+impl Drop for Creature {
+    fn drop(&mut self) {
+        println!("{} is dead", self.name);
+    }
+}
+
+fn main() {
+    let mut clever: Creature;
+    {
+        let goblin = Creature::new("Jeff");
+        println!("game proceeds");
+        clever = goblin;
+        println!("end of scope");
+        // drop(goblin);
+    }
+}
