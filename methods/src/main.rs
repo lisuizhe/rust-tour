abed008c71b9aa32365bf6968efea1910d5d9ce7
lisuizhe <suizhe.li@outlook.com>
2022-06-19 16:26:20 +0900

    methods

---
+++ methods/src/main.rs
@@ -0,0 +1,29 @@
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+impl Line {
+    fn len(&self) -> f64 {
+        let dx = self.start.x - self.end.x;
+        let dy = self.start.y - self.end.y;
+        (dx * dx + dy * dy).sqrt()
+    }
+}
+
+fn methods() {
+    let p = Point { x: 3.0, y: 4.0 };
+    let p2 = Point { x: 5.0, y: 10.0 };
+    let line = Line { start: p, end: p2 };
+
+    println!("length = {}", line.len());
+}
+
+fn main() {
+    methods();
+}
